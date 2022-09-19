e037bc44d00cd16c9173adb2c36011f67906f662
sol3000 <79767805+sol3000@users.noreply.github.com>
2022-09-19 11:57:13 +0200

    Add files via upload

---
+++ MiCalculadora/src/micalculadora/main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package micalculadora;
+
+public class main {
+
+ 
+       public static void main(String[] args) {
+             MiCalculadora calculadora = new MiCalculadora();
+             calculadora.setLocation(100, 100);
+             calculadora.setVisible(true);
+             
+       }
+}
